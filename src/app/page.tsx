899b9758a6797a23b0ed3748ab8b5a8d393b8f96
Raficknew <rafi20302@gmail.com>
2025-03-07 22:33:40 +0100

    clerk setuped

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,19 @@
-import { SignInButton } from "@clerk/nextjs";
+import { getCurrentUser } from "@/services/clerk";
 import { Suspense } from "react";
 
-export default function HomePage() {
+export default async function HomePage() {
   return (
-    <Suspense>
-      <SignInButton>Sign in</SignInButton>
-    </Suspense>
+    <div className="min-h-screen min-w-screen flex justify-center items-center">
+      <Suspense>
+        <UserHouseHolds />
+      </Suspense>
+    </div>
   );
+}
+
+async function UserHouseHolds() {
+  const { userId, redirectToSignIn } = await getCurrentUser();
+  if (userId == null) redirectToSignIn();
+
+  return <div>Moje gospodarstwa domowe</div>;
 }
