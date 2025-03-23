f1cbcb10f1c4abd8288d32beb5a98faf654cf65b
Raficknew <rafi20302@gmail.com>
2025-03-23 21:05:21 +0100

    Form for creating households

--- src/app/[locale]/(houseHold)/create/page.tsx
+++ src/app/[locale]/(houseHold)/create/page.tsx
@@ -1,7 +1,19 @@
-export default async function HouseHoldPage() {
+import { db } from "@/drizzle";
+import { CurrencyTable } from "@/drizzle/schema";
+import { HouseHoldForm } from "@/features/houseHold/components/HouseHoldForm";
+
+export default async function CreateHouseHoldPage() {
+  const currencies = await getCurrencies();
   return (
-    <div>
-      <p>Tworzę gospodarstwo</p>
+    <div className="flex justify-center h-screen w-full items-center">
+      <div className="flex flex-col justify-center grow items-center max-w-[400px] bg-card p-6 text-center rounded-sm gap-y-5 max-h-[480px] border-foreground border ">
+        <h1 className="text-2xl">Utwórz nowe gospodarstwo</h1>
+        <HouseHoldForm currencies={currencies} />
+      </div>
     </div>
   );
+}
+
+function getCurrencies() {
+  return db.selectDistinct().from(CurrencyTable);
 }
---
+++ src/features/houseHold/components/HouseHoldForm.tsx
@@ -0,0 +1,111 @@
+"use client";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { houseHoldSchema } from "../schema/houseHolds";
+import { insertHouseHold } from "../actions/houseHolds";
+
+export function HouseHoldForm({
+  currencies,
+}: {
+  currencies: { code: string }[];
+}) {
+  const form = useForm<z.infer<typeof houseHoldSchema>>({
+    resolver: zodResolver(houseHoldSchema),
+    defaultValues: {
+      name: "",
+      description: "",
+      currencyCode: "",
+    },
+  });
+
+  async function onSubmit(data: z.infer<typeof houseHoldSchema>) {
+    await insertHouseHold(data);
+  }
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="grow w-full space-y-8"
+      >
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Nazwa</FormLabel>
+              <FormControl>
+                <Input
+                  placeholder="Wpisz nazwę dla nowego gospodarstwa"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="description"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Opis</FormLabel>
+              <FormControl>
+                <Input placeholder="Wpisz opis dla gospodarstwa" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="currencyCode"
+          render={({ field }) => (
+            <FormItem className="flex">
+              <FormLabel>Waluta</FormLabel>
+              <FormControl>
+                <Select
+                  onValueChange={field.onChange}
+                  defaultValue={field.value}
+                >
+                  <SelectTrigger className="w-[180px]">
+                    <SelectValue placeholder="Wybierz walutę" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    {currencies.map((currency) => (
+                      <SelectItem key={currency.code} value={currency.code}>
+                        {currency.code}
+                      </SelectItem>
+                    ))}
+                  </SelectContent>
+                </Select>
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button variant="submit" type="submit">
+          Submit
+        </Button>
+      </form>
+    </Form>
+  );
+}
