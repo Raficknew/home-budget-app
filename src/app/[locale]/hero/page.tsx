6b1ec064e2f0e174b4e7ff81c102c5206ac5439a
Rafał <rafi20302@gmail.com>
2025-10-02 10:20:33 +0200

    hero page done

--- src/app/[locale]/hero/page.tsx
+++ src/app/[locale]/hero/page.tsx
@@ -24,19 +24,19 @@ export default async function HeroPage({
         className="fixed top-18 sm:top-7.5 sm:right-8"
       />
 
-      <div className="flex flex-col items-center justify-between h-full pt-50 xl:w-[1280px] lg:w-[1024px] md:w-[768px] sm:w-[540px]">
-        <div className="flex flex-col xl:text-7xl lg:text-5xl md:text-4xl font-semibold">
+      <div className="flex flex-col items-center justify-between h-full pt-50 xl:w-[1280px] lg:w-[1024px] md:w-[768px] sm:w-[540px] w-full px-3">
+        <div className="flex flex-col xl:text-6xl lg:text-5xl md:text-4xl sm:text-2xl text-xl font-semibold sm:text-start text-center">
           <h1 className="text-[#BDB6FC]">{t("firstTerm")}</h1>
-          <h1 className="indent-15 bg-gradient-to-r from-[#7047EB] to-[#BDB6FC] bg-clip-text text-transparent w-fit inline-block leading-[1.4] align-text-bottom">
+          <h1 className="sm:self-start self-center sm:indent-15 bg-gradient-to-r from-[#7047EB] to-[#BDB6FC] bg-clip-text text-transparent w-fit inline-block leading-[1.4] align-text-bottom">
             {t("secondTerm")}
           </h1>
-          <div className="flex flex-col gap-2">
-            <h1 className="text-[#7047EB] indent-30">{t("thirdTerm")}</h1>
-            <div className="text-end flex flex-col gap-2 lg:text-base md:text-sm font-normal">
+          <div className="flex flex-col sm:gap-2 gap-4">
+            <h1 className="text-[#7047EB] sm:indent-30">{t("thirdTerm")}</h1>
+            <div className="sm:text-end text-center flex flex-col sm:gap-2 gap-4 lg:text-base md:text-sm text-xs font-normal">
               <h2 className="text-white/70">{t("description")}</h2>
               <Link
                 className={cn(
-                  "self-end",
+                  "sm:self-end",
                   buttonVariants({ variant: "submit" })
                 )}
                 href={`/${locale}/sign-in`}
