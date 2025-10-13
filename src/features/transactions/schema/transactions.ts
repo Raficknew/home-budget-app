e6f45eb0e5f5b3f05bf6cf09284358ef85b2bf3e
Rafał <rafi20302@gmail.com>
2025-10-13 21:09:41 +0200

    changed a minimum signs for name

--- src/features/transactions/schema/transactions.ts
+++ src/features/transactions/schema/transactions.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 export const transactionsSchema = z.object({
   price: z.coerce.number().min(0),
   type: z.string().min(1),
-  name: z.string().min(0),
+  name: z.string().min(3),
   memberId: z.string().min(1),
   date: z.date(),
   categoryId: z.string().min(1),
